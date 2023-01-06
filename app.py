55bbe5e8a51f956df9262191cb728b0afebde9d4
RaviPantar <49116856+RaviPantar@users.noreply.github.com>
2023-01-06 19:35:03 +0530

    Updated

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
-@app.route("/")
+
+@app.route('/')
 def hello():
-  return "Hello World!"
-if __name__ == "__main__":
-  app.run(host="0.0.0.0",port=int("5000"),debug=True)
+   return render_template('index.html')
+
+@app.route('/hello/<name>')
+def hello_name(name):
+   # Load current count
+   f = open("count.txt", "r")
+   count = int(f.read())
+   f.close()
+   # Increment the count
+   count += 1
+   # Overwrite the count
+   f = open("count.txt", "w")
+   f.write(str(count))
+   f.close()
+
+   return render_template('hello.html', name=name, count=count)
+
+if __name__ == '__main__':
+   app.run(host ='0.0.0.0', debug = True)
